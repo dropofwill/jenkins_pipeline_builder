3f95ccc71cfd6160a65861c208e47b9012812268
wdoan <wdoan@constantcontact.com>
2016-10-12 09:36:44 -0400

    CD-855: clean up unecessary spaces

--- spec/lib/jenkins_pipeline_builder/extensions/conditions_spec.rb
+++ spec/lib/jenkins_pipeline_builder/extensions/conditions_spec.rb
@@ -51,29 +51,61 @@ describe 'conditions' do
   end
 
   context 'type: manual' do
-    before :each do 
+    before :each do
       allow(JenkinsPipelineBuilder.client).to receive(:plugin).and_return double(
         list_installed: { 'promoted_builds' => '2.31' }
       )
     end
 
-    it 'generates manual configuration' do 
-      params = { conditions: [ { manual: { users: 'authorized' } } ] }
+    it 'generates manual configuration given parameter' do
+      params = { conditions: [ { manual: { users: 'unauthorized' } } ] }
+      
+      JenkinsPipelineBuilder.registry.traverse_registry_path('job', params, @n_xml)
+      
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.ManualCondition/users').text).to eq('unauthorized')
+    end
+
+    it 'generates manual configuration defaults' do
+      params = { conditions: [ { manual: { } } ] }
       
       JenkinsPipelineBuilder.registry.traverse_registry_path('job', params, @n_xml)
       
       expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.ManualCondition/users').text).to eq('authorized')
     end
+  end
 
-    it 'generates a self promotion configuration' do 
+  context 'type: self_promotion' do
+    before :each do
+      allow(JenkinsPipelineBuilder.client).to receive(:plugin).and_return double(
+        list_installed: { 'promoted_builds' => '2.31' }
+      )
+    end
+
+    it 'generates a self promotion configuration given parameters' do
       params = { conditions: [ { self_promotion: { even_if_unstable: true } } ] }
       
       JenkinsPipelineBuilder.registry.traverse_registry_path('job', params, @n_xml)
-      
+
       expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.SelfPromotionCondition/even_if_unstable').text).to eq('true')
     end
 
-    it 'generates a parameterized self promotion configuration' do 
+    it 'generates a self promotion configuration defaults' do
+      params = { conditions: [ { self_promotion: { } } ] }
+      
+      JenkinsPipelineBuilder.registry.traverse_registry_path('job', params, @n_xml)
+      
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.SelfPromotionCondition/even_if_unstable').text).to eq('false')
+    end
+  end
+
+  context 'type: parameterized_self_promotion' do
+    before :each do
+      allow(JenkinsPipelineBuilder.client).to receive(:plugin).and_return double(
+        list_installed: { 'promoted_builds' => '2.31' }
+      )
+    end
+
+    it 'generates a parameterized self promotion configuration given parameters' do
       params = { conditions: [ { parameterized_self_promotion: { parameter_name: 'SOME_ENV_CHAR', parameter_value: true, even_if_unstable: true } } ] }
       
       JenkinsPipelineBuilder.registry.traverse_registry_path('job', params, @n_xml)
@@ -83,7 +115,25 @@ describe 'conditions' do
       expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.ParameterizedSelfPromotionCondition/even_if_unstable').text).to eq('true')
     end
 
-    it 'generates a downstream pass configuration' do 
+    it 'generates a parameterized self promotion configuration defaults' do
+      params = { conditions: [ { parameterized_self_promotion: { } } ] }
+      
+      JenkinsPipelineBuilder.registry.traverse_registry_path('job', params, @n_xml)
+
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.ParameterizedSelfPromotionCondition/parameter_name').text).to eq('')
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.ParameterizedSelfPromotionCondition/parameter_value').text).to eq('false')
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.ParameterizedSelfPromotionCondition/even_if_unstable').text).to eq('false')
+    end
+  end
+
+  context 'type: downstream_pass' do
+    before :each do
+      allow(JenkinsPipelineBuilder.client).to receive(:plugin).and_return double(
+        list_installed: { 'promoted_builds' => '2.31' }
+      )
+    end
+
+    it 'generates a downstream pass configuration given parameters' do
       params = { conditions: [ downstream_pass: { jobs: "Worst-Commit-Ever", even_if_unstable: true } ] }
       
       JenkinsPipelineBuilder.registry.traverse_registry_path('job', params, @n_xml)
@@ -92,12 +142,52 @@ describe 'conditions' do
       expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.DownstreamPassCondition/even_if_unstable').text).to eq('true')
     end
 
-    it 'generates a upstream promotion configuration' do 
+    it 'generates a downstream pass configuration defaults' do
+      params = { conditions: [ downstream_pass: { } ] }
+
+      JenkinsPipelineBuilder.registry.traverse_registry_path('job', params, @n_xml)
+
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.DownstreamPassCondition/jobs').text).to eq('{{Example}}-Commit')
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.DownstreamPassCondition/even_if_unstable').text).to eq('false')
+    end
+  end
+
+  context 'type: upstream_promotion' do
+    before :each do
+      allow(JenkinsPipelineBuilder.client).to receive(:plugin).and_return double(
+        list_installed: { 'promoted_builds' => '2.31' }
+      )
+    end
+
+    it 'generates a upstream promotion configuration parameters' do
       params = { conditions: [ upstream_promotion: { promotion_name: 'Never-Promotes' } ] }
       
       JenkinsPipelineBuilder.registry.traverse_registry_path('job', params, @n_xml)
       
       expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.UpstreamPromotionCondition/promotion_name').text).to eq('Never-Promotes')
+    end
+
+    it 'generates a upstream promotion configuration defaults' do
+      params = { conditions: [ upstream_promotion: { } ] }
+
+      JenkinsPipelineBuilder.registry.traverse_registry_path('job', params, @n_xml)
+
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.UpstreamPromotionCondition/promotion_name').text).to eq('01. Staging Promotion')
+    end
+
+    it 'generates all condition defaults given no parameters' do
+      params = { conditions: [ { manual: { } }, { self_promotion: { } }, { parameterized_self_promotion: { } }, { downstream_pass: { } }, { upstream_promotion: { } } ] }
+
+      JenkinsPipelineBuilder.registry.traverse_registry_path('job', params, @n_xml)
+
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.ManualCondition/users').text).to eq('authorized')
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.SelfPromotionCondition/even_if_unstable').text).to eq('false')
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.ParameterizedSelfPromotionCondition/parameter_name').text).to eq('')
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.ParameterizedSelfPromotionCondition/parameter_value').text).to eq('false')
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.ParameterizedSelfPromotionCondition/even_if_unstable').text).to eq('false')
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.DownstreamPassCondition/jobs').text).to eq('{{Example}}-Commit')
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.DownstreamPassCondition/even_if_unstable').text).to eq('false')
+      expect(@n_xml.at('//hudson.plugins.promoted__builds.conditions.UpstreamPromotionCondition/promotion_name').text).to eq('01. Staging Promotion')
     end
   end
 end
